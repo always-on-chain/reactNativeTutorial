a06820f924562e2da05b0039a24f0926efaa170c
waynekwest88@gmail.com <waynekwest88@gmail.com>
2019-05-08 18:22:41 -0700

    installed redux and react-redux and set up reducer boiler plate

---
+++ src/store/reducers/places.js
@@ -0,0 +1,12 @@
+const initialState = {
+  places: [],
+  selectedPlaces: null
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
